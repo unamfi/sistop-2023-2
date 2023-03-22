dfdc6fb80f7ebeed2e535f16a7a36c072f0c05b3
Jorge Kareem Rosado Dominguez <jkrd1995@gmail.com>
2023-03-22 17:23:57 -0600

    Programa semaforo

--- practicas/3/semaforo.c
+++ practicas/3/semaforo.c
@@ -1,94 +1,20 @@
 #include <stdio.h>
-#include <errno.h>
+#include <unistd.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/ipc.h>
-#include <sys/sem.h>
-
-/****************************************************************
-Funciones auxiliares para inicializar, hacer wait y hacer Signal
-Funcionan con arrays de semaforos, si solo hay uno ese parametro es 0
-*******************************************************************/
-void error(char* errorInfo) {
-    fprintf(stderr,"%s",errorInfo);
-    exit(1);
-}
-
-void doSignal(int semid, int numSem) {
-    struct sembuf sops; //Signal
-    sops.sem_num = numSem;
-    sops.sem_op = 1;
-    sops.sem_flg = 0;
-
-    if (semop(semid, &sops, 1) == -1) {
-        perror(NULL);
-        error("Error al hacer Signal");
-    }
-}
-
-void doWait(int semid, int numSem) {
-    struct sembuf sops;
-    sops.sem_num = numSem; /* Sobre el primero, ... */
-    sops.sem_op = -1; /* ... un wait (resto 1) */
-    sops.sem_flg = 0;
-
-    if (semop(semid, &sops, 1) == -1) {
-        perror(NULL);
-        error("Error al hacer el Wait");
-    }
-}
-
-void initSem(int semid, int numSem, int valor) { //iniciar un semaforo
-  
-    if (semctl(semid, numSem, SETVAL, valor) < 0) {        
-    perror(NULL);
-        error("Error iniciando semaforo");
-    }
-}
-
 int main() {
-    puts("Sincronizacion con Semaforos ");
-    int semaforo;
-    
-
-    //Manera de usar semget http://pubs.opengroup.org/onlinepubs/7908799/xsh/semget.html
-    //Creamos un semaforo y damos permisos para compartirlo
-    if((semaforo=semget(IPC_PRIVATE,1,IPC_CREAT | 0700))<0) {
-        perror(NULL);
-        error("Semaforo: semget");
-        }
-
-    initSem(semaforo,0,1);
-    puts("Hay una plaza libre");  
-
-    switch (fork())
-    {
+    pid_t rf;
+    rf = fork();
+    switch(rf){
         case -1:
-            error("Error en el fork"); 
-
-        case 0:  /* Hijo */
-            doWait(semaforo,0);
-            puts("Entro el hijo, el padre espera");
-            sleep(5);
-            puts("El hijo sale");
-            doSignal(semaforo,0);
-            exit(0);
-
-        default: /* Padre */
-            doWait(semaforo,0);
-            puts("Entro el padre, el hijo espera");
-            sleep(5);
-            puts("El padre sale");
-            doSignal(semaforo,0);
-    }       
-    
-    sleep(20);
-    
-    //Liberacion del semaforo
-    if ((semctl(semaforo, 0, IPC_RMID)) == -1) {
-        perror(NULL);
-        error("Semaforo borrando");
+        printf("no he podido crear el prceso del hijo \n");
+        break;
+        case 0:
+        printf("Soy el hijo, mi PID es %d y mi PID es %d \n",getpid(),getpid());
+        sleep(2);
+        break;
+        default:
+        printf("Soy el padre, mi PID es %d y el PID de mi hijo es  %d \n",getpid(),rf);
+        sleep(2);
     }
-    return 0;
+    printf("Final de ejecucion de %d \n",getpid());
 }
