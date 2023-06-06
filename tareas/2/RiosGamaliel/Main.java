b61c3abeece0e197546c4a94e07b66dfc70ee778
GamaRL <gamarl1973@gmail.com>
2023-06-05 23:58:09 -0600

    ADD: Se completa la tarea 2

--- tareas/2/RiosGamaliel/Main.java
+++ tareas/2/RiosGamaliel/Main.java
@@ -12,13 +12,17 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
+  // El directorio a analizar
   private static File directory = null;
+
+  // El límite de días de modificación máxima
   private static long maxDays;
 
+  // Obtiene los parámetros que se pasaron desde la línea de comandos
   private static boolean getParams(String[] args) {
     File file = new File(args[0]);
     Main.maxDays = Long.parseLong(args[1]);
-    
+
     if (Main.maxDays >= 0 && !file.exists() || !file.isDirectory()) {
       return false;
     }
@@ -27,6 +31,8 @@ public class Main {
     return true;
   }
 
+  // Obteiene todos los archivos editados en el intervalo de la fecha actual y
+  // `maxDays` hacia atrás.
   private static List<File> listDirectoryFiles() {
     var today = LocalDateTime.now(ZoneId.systemDefault());
     FileFilter filter = (f) -> {
@@ -39,10 +45,13 @@ public class Main {
     return List.of(directory.listFiles(filter));
   }
 
+  // Dado un archivo, obtiene una cadena que representa los permisos del
+  // archivo en octal.
   private static String getOctalPermissions(File file) throws IOException {
     int[] modes = {0,0,0};
     var filePermissions = Files.getPosixFilePermissions(file.toPath());
 
+    // Para el dueño
     if (filePermissions.contains(PosixFilePermission.OWNER_READ))
       modes[0] += 4;
     if (filePermissions.contains(PosixFilePermission.OWNER_WRITE))
@@ -50,6 +59,7 @@ public class Main {
     if (filePermissions.contains(PosixFilePermission.OWNER_EXECUTE))
       modes[0] += 1;
 
+    // Para el grupo del duaño
     if (filePermissions.contains(PosixFilePermission.GROUP_READ))
       modes[1] += 4;
     if (filePermissions.contains(PosixFilePermission.GROUP_WRITE))
@@ -57,6 +67,7 @@ public class Main {
     if (filePermissions.contains(PosixFilePermission.GROUP_EXECUTE))
       modes[1] += 1;
 
+    // Para los demás usuarios
     if (filePermissions.contains(PosixFilePermission.OTHERS_READ))
       modes[2] += 4;
     if (filePermissions.contains(PosixFilePermission.OTHERS_WRITE))
@@ -64,36 +75,41 @@ public class Main {
     if (filePermissions.contains(PosixFilePermission.OTHERS_EXECUTE))
       modes[2] += 1;
 
+    // ¿Es archivo o directorio?
     String initialSequence = file.isFile() ? "100" : "040";
 
     return initialSequence + modes[0] + modes[1] + modes[2];
 
   }
 
+
+  // Ordena los archivos por nombre (No toma en cuenta las mayúsculas y 
+  // minúsculas)
   private static List<File> sortFiles(List<File> files) {
     return files
-        .stream()
-        .parallel()
-        .sorted((a, b) -> {
-          return a.getName().toLowerCase().compareTo(b.getName().toLowerCase());
-        })
-        .collect(Collectors.toList());
+      .stream()
+      .parallel()
+      .sorted((a, b) -> {
+        return a.getName().toLowerCase().compareTo(b.getName().toLowerCase());
+      })
+    .collect(Collectors.toList());
   }
 
+  // Muestra la información de la lista de archivos en forma tabular
   private static void showFilesInformation(List<File> files) {
     System.out.printf("%25s\t%18s\t%6s\t%10s\t\n",
-      "Nombre", "Modificación", "Modo", "Tamaño");
+        "Nombre", "Modificación", "Modo", "Tamaño");
     System.out.printf("%60s\n",
-      "=====================================" + 
-      "=====================================");
+        "=====================================" + 
+        "=====================================");
     files
       .forEach(f -> {
         String nameStr = f.getName();
         nameStr = nameStr.length() > 25 ? nameStr.substring(0, 22).concat("...") : nameStr;
         String dateStr = LocalDateTime
           .ofInstant(
-            Instant.ofEpochMilli(f.lastModified()),
-            ZoneId.systemDefault())
+              Instant.ofEpochMilli(f.lastModified()),
+              ZoneId.systemDefault())
           .format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:ss"));
         String modeStr = "[]";
         try {
@@ -103,17 +119,18 @@ public class Main {
         }
         String size = String.valueOf(f.length());
         System.out.printf("%25s\t%18s\t%6s\t%10s\n",
-          nameStr, dateStr, modeStr, size);
+            nameStr, dateStr, modeStr, size);
       });
   }
 
   public static void main(String... args) {
     if (args.length == 2 && getParams(args)) {
-        System.out.printf("Showing %s directory", directory);
-        var filteredFiles = sortFiles(listDirectoryFiles());
-        showFilesInformation(filteredFiles);
-        return;
-      }
+      System.out.printf("Mostrando: %s\n", directory);
+      System.out.printf("Última modificación: Max. %d días\n\n", maxDays);
+      var filteredFiles = sortFiles(listDirectoryFiles());
+      showFilesInformation(filteredFiles);
+      return;
+    }
     System.err.append("Perdón, esto no es válido :(");
   }
 }
--- tareas/2/RiosGamaliel/README.md
+++ tareas/2/RiosGamaliel/README.md
@@ -26,11 +26,56 @@ Con esto, `Java` compilará y ejecutará de forma automática el programa.
 
 ## Descripción del programa
 
+La mayoría de la lógica que proporciona `Java` para el manejo de archivos, se 
+encuentra dentro de la clase `File` del paquete `java.io`.
+
+El funcionamiento de programa realizado consta de las siguientes etapas:
+- *Obtención y validación de los parámetros*: En esta etapa, se valida que los 
+parámetros de ejecución sean válidos. Por una parte, se verifica que el 
+directorio listado exista y que efectivamente sea un directorio. Además, se 
+toma el valor de los días para filtrar. Para esto, se colocó la lógica 
+dentro del método `getParams()`.
+- *Obtener los archivos dentro del directorio*: Para esto, se hace uso del 
+método `listFiles()` de la clase `File`. Se le aplica un filtro a través de 
+la clase `FileFilter` y a través de las clases de _tiempo y fecha_ que 
+proporciona el lenguaje se calcula el número de días de la última modificación 
+del archivo. Esto se encapsuló dentro del método `listDirectoryFiles()`.
+- *Ordenar los archivos con base en su nombre*: Para esto, no se hizo más que 
+aplicar un algorimo de ordenamiento a la lista obtenida en el paso anterior 
+con ayuda de una herramienta llamada _streams_ que proporciona el lenguaje. La 
+comparación se implementó de forma no sensitiva a letras mayúsculas a través 
+del nombre del archivo. Esto se implementó dentro del método `sortFiles()`.
+- *Mostrar la información*: Se itera sobre la lista de archivos y se va 
+listando la información solicitada en forma de tabular. En esta sección, se 
+implementó el método auxiliar `getOctalPermissions()` con la finalidad de 
+obtener la cadena mostrada en las instrucciones. La lógica de este listado 
+de muestra dentro de `showFilesInformation()`.
+
+Con todo lo anterior, la ejecución del programa resultó en:
+
+```bash
+$ java Main.java /home/gamarl/sistop-2023-2/tareas/2/ 50
+```
+<pre>
+Mostrando: /home/gamarl/sistop-2023-2/tareas/2/
+Última modificación: Max 10 days
+
+                   Nombre	      Modificación	  Modo	    Tamaño	
+==========================================================================
+                README.md	  30-05-2023 22:00	100664	      3220
+             RiosGamaliel	  05-06-2023 23:41	040775	      4096
+</pre>
+
 ---
 ## Comentarios adicionales
-Por más que estuve investigando a profundidad, no encontré cómo mostrar en formato octal 
-el modo de cada archivo. Lo más cercano que se encontró fue el método `Files.getPosixFilePermissions()`,
-el cual devuelve los permisos y a través de ellos se obtener el formato octal haciendo cada una 
-de las comparaciones necesarias. Además, en un comienzo no entendí bien qué eran los primeros tres dígitos al comienzo del formato de modo mostrado en la captura. Investigando un poco más, pude concluir que se tiene:
+Por más que estuve investigando a profundidad, no encontré cómo mostrar en 
+formato octal el modo de cada archivo. Lo más cercano que se encontró fue el 
+método `Files.getPosixFilePermissions()`, el cual devuelve los permisos y a 
+través de ellos se obtener el formato octal haciendo cada una de las 
+comparaciones necesarias. Además, en un comienzo no entendí bien qué eran los 
+primeros tres dígitos al comienzo del formato de modo mostrado en la captura.  
+Investigando un poco más, pude concluir que se tiene:
 - `100`: Para indicar que es un archivo
 - `040`: Para indicar que es un directorio
+
+Simplemente realicé una comparación para gregar la cadena correspondiente.
