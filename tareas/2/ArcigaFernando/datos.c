b520d1384f32e962dcc2427248148c3717dbf32d
Fernando Arciga <arcigaguzmanfernando@gmail.com>
2023-06-06 15:24:12 -0600

    Bug: todavía no da los datos bien

---
+++ tareas/2/ArcigaFernando/datos.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <time.h>
+
+typedef struct stat _stat;
+typedef struct dirent _dirent;
+
+void read(_stat);
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) return 1;
+    
+    char *nombredir = argv[1];
+    DIR *dir; dir = opendir(nombredir);
+    if (dir == NULL) {
+        printf("\nNo se pudo abrir el directorio '%s'.\n", nombredir);
+        return 1;
+    }
+
+    long int dias;
+    sscanf(argv[2], "%ld", &dias);
+    time_t segundos = dias * 24 * 60 * 60;
+    time_t eloy = time(NULL);
+
+    _dirent *archivo;
+    while ((archivo = readdir(dir)) != NULL) {
+        char ruta[257];
+        snprintf(ruta, sizeof(ruta), "%s/%s", nombredir, archivo->d_name);
+        _stat file_info;
+        if (stat(ruta, &file_info) == 0){ // no hay error al intentar abrir el archivo
+            time_t edad = eloy - file_info.st_mtime;
+            if((file_info.st_mode != S_IFDIR)){
+                if (edad <= segundos){
+                    printf("\nArchivo: %s\t%d\t", archivo->d_name, edad/24/60/60);
+                    read(file_info);
+                }
+            }
+        }
+    }
+
+    closedir(dir);
+}
+
+void read(_stat file_info) {
+    printf("\nTamaño del archivo: %ld bytes", file_info.st_size);
+    printf("\nÚltimo acceso: %d", ctime(&file_info.st_atime));
+    printf("\nÚltima modificación: %d", ctime(&file_info.st_mtime));
+    printf("\nÚltimo cambio de estado: %d\n", ctime(&file_info.st_ctime));
+}
